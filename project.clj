be12775da93deaa0aaf502255eb18c4e10bfebaf
Paul Lam <paul@quantisan.com>
2013-03-20 20:59:16 +0000

    url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-polling "1.0.0-SNAPSHOT"
   :description "A Clojure library for polling resources."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/Quantisan/clj-polling"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]])
