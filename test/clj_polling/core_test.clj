2e1194723a7ae8942bd827b3c5db78b431a1306c
Paul Lam <paul@quantisan.com>
2013-03-20 20:48:53 +0000

    test periodically and shutdown

--- test/clj_polling/core_test.clj
+++ test/clj_polling/core_test.clj
@@ -2,6 +2,16 @@
   (:use clojure.test
         clj-polling.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test-atom (atom 0))
+
+(defn inc-atom [x]
+  (swap! x inc))
+
+(deftest periodic-inc-test
+  (testing "Periodically inc test-atom, shutdown after 3 periods"
+    (is (= 3
+           (dosync
+             (periodically (partial inc-atom test-atom) 100 200)
+             (Thread/sleep 600)
+             (shutdown)
+             @test-atom)))))
